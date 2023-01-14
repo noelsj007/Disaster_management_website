1142a5c88235da0d4394ef10ebb2c5b04164584f
Dark OP <noel.s.jacob2002@gmail.com>
2023-01-14 14:38:42 +0530

    volunteer addition

--- Dmgt/settings.py
+++ Dmgt/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'disaster',
+    'volunteer',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'volunteer.CustomUser'
---
+++ volunteer/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    
+    username = None
+    email = models.EmailField(('email address'), unique=True)
+    mobile = models.CharField(max_length=15, null=True, blank= True)
+    is_verified = models.BooleanField(default=False)
+    forget_password = models.CharField(max_length=255, blank= True, null=True)
+    
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['mobile']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.first_name
